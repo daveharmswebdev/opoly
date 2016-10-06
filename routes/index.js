ae8caa64e553a2d274dc569fc174f7bf8cf60d11
Dave Harms <daveharmswebdev@gmail.com>
2016-10-06 10:44:48 -0500

    created dev environment, express listens, mongo connect good, require socket infrastructure

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const { Router } = require('express')
+const router = Router()
+
+router.get('/', (req,res) => {
+	res.send('hello dave')
+})
+
+module.exports = router
---
+++ server/server.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const express = require('express')
+const { Server } = require('http')
+const socketio = require('socket.io')
+const mongoose = require('mongoose')
+
+const app = express()
+const server = Server(app)
+const io = socketio(server)
+
+const routes = require('../routes')
+
+// constants
+const PORT = process.env.PORT || 3000
+const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/opoly'
+
+// middleware
+app.set('view engine', 'pug')
+app.use(express.static('client'))
+
+app.use(routes)
+
+mongoose.Promise = Promise
+mongoose.connect(MONGODB_URL, () => {
+	server.listen(PORT, () => {
+		console.log(`now listening on ${PORT}`)
+	})
+})
